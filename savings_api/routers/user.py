2d651140c16d7b98853a281fa41b0bc7f001811e
Lance Sparks <Sparks@RR19-MB190004.local>
2026-05-06 11:33:48 -0400

    sign in page created

--- savings_api/routers/user.py
+++ savings_api/routers/user.py
@@ -107,7 +107,6 @@ def create_user( db: db_dependency, user: CreateUserRequest):
 async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
 ):
-    print('asdsaassadasd')
     user = authenticate_user(form_data.username, form_data.password, db)
     print(form_data)
     if not user:
